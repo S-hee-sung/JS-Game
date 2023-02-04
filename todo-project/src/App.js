008017f80817ad6aae61124e04c13b8219e81609
S-hee-sung <dlsglrhdwjs@naver.com>
2023-02-05 03:22:47 +0900

    02.05 1차 수정 커밋

--- todo-project/src/App.js
+++ todo-project/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useRef, useState } from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoInsert from "./component/TodoInsert";
+import TodoList from "./component/TodoList";
+import TodoTemplate from "./component/TodoTemplate";
+import { reset } from "styled-reset";
+import TodoHead from "./component/TodoHead";
+import { v4 as uuidv4 } from "uuid";
+
+
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '수업 교안 작성하기',
+      checked: true
+    },
+    {
+      id: 2,
+      text: '시험 채점하기',
+      checked: true
+    },
+    {
+      id: 3,
+      text: '단계별 실습 예제 만들기',
+      checked: false
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+  const handleInsert = useCallback((text) => {
+    const todo = {
+      id: uuidv4(),
+      text,
+      checked: false
+    };
+    setTodos(todos.concat(todo)); 
+
+    nextId.current += 1; 
+  
+  },[todos]);
+
+  const handleRemove = useCallback((id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  } ,[todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList todos={todos} />
+        <TodoInsert onInsert={handleInsert} onRemove={handleRemove}/>
+      </TodoTemplate>
+    </>  
   );
 }
 
---
+++ todo-project/src/component/TodoList.jsx
@@ -0,0 +1,26 @@
+import TodoListItem from "./TodoListItem";
+import styled from "styled-components";
+
+const TodoListWrapper = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+`;
+
+function TodoList({ todos, onRemove }) {
+  return (  
+    <TodoListWrapper>
+      {todos.map((todo) =>{
+        return(
+          <TodoListItem 
+            key={todo.id} 
+            todo={todo}
+            onRemove={onRemove} 
+          />
+        );
+      })}
+    </TodoListWrapper>
+  );
+}
+
+export default TodoList;
